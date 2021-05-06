9bd77018395bf94e2e86154cbb7e68c3d1d8be76
suengwoo Lee <na1yeon33@gmail.com>
2021-05-07 00:48:14 +0900

    HelloC#World

---
+++ HelloCsharpWorld/HelloCsharpWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic; // 폴더의 경로
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCsharpWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("안녕 C# 월드");          
+            Console.WriteLine("Hello C# World!"); // Line : 줄바꿈 기능
+        }
+    }
+}
