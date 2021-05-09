ef22f7bb04880b11ee3fd43fed3b423432b90f74
suengwoo Lee <na1yeon33@gmail.com>
2021-05-09 18:49:19 +0900

    if문,switch문

---
+++ Chap04_01/Chap04_01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Chap04_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int z = 5;
+
+            if (z == 1) Console.WriteLine("숫자 1입니다.");
+            else if (z == 10) Console.WriteLine("숫자 10입니다.");
+            else Console.WriteLine("case에 없는 값일 때 실행!");
+
+            //switch(z)
+            //{
+            //    case 1:
+            //        Console.WriteLine("숫자 1입니다.");
+            //        break; // 탈출문
+            //    case 10:
+            //        Console.WriteLine("숫자 10입니다.");
+            //        break;
+            //    default:
+            //        Console.WriteLine("case에 없는 값일 때 실행!");
+            //        break;
+            //}
+
+            //int x = 3, y = 7;
+
+            //if (x > 7)          
+            //    Console.WriteLine("if문이 참이면 실행!");
+            //else if(x != y)
+            //    Console.WriteLine("첫번째 if문이 False일때, 2번째 if문이 True일때 실행!");
+            //else
+            //    Console.WriteLine("이곳은 출력이 될까요???");           
+        }
+    }
+}
