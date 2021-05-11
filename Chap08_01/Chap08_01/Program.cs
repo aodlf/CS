eae242d1ea75eaa3f5f7d0b034b80d1958284900
suengwoo Lee <na1yeon33@gmail.com>
2021-05-11 22:30:51 +0900

    프로퍼티/ 배열/ 컬렉션 -1

---
+++ Chap08_01/Chap08_01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chap08_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
